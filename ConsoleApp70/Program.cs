0d244438998f88fefdfa0a31eb195b8173e9b95e
Voloshina Sofiya <voloshina_sofiya@mail.ru>
2021-06-15 14:20:46 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp70/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp70
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
